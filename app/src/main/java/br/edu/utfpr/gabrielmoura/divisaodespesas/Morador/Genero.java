d141134821eab35173e970f499814031aab3cfc0
Gabriel Moura <gmendesmoura@gmail.com>
2026-02-26 17:06:23 -0300

    remove: Cria novas classes, renomea e altera o caminho dos arquivos anteriormente criados.

---
+++ app/src/main/java/br/edu/utfpr/gabrielmoura/divisaodespesas/Morador/Genero.java
@@ -0,0 +1,4 @@
+package br.edu.utfpr.gabrielmoura.divisaodespesas.Morador;
+
+public enum Genero {
+}
---
+++ app/src/main/java/br/edu/utfpr/gabrielmoura/divisaodespesas/Morador/Morador.java
@@ -0,0 +1,4 @@
+package br.edu.utfpr.gabrielmoura.divisaodespesas.Morador;
+
+public class Morador {
+}
